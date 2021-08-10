f84c92ead7828d0aea7c07aadad70793c7e1a11e
zarrix <souf.zarr@outlook.com>
2021-08-10 13:27:57 +0100

    update requests api

---
+++ controller/requestController.js
@@ -0,0 +1,44 @@
+
+// Models
+const Farmer = require('../models/farmer');
+const Association = require('../models/association');
+const School = require('../models/school')
+
+//get nurseries statistics
+module.exports.getStatistics = async (req, res) => {
+
+    let info = {};
+
+    await Farmer.countDocuments({}, function(err, c) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err.message)
+        } else {
+            info["requests"] = c
+            info["farmers"] = c;
+        }
+    });
+
+    await Association.countDocuments({}, function(err, c) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err.message)
+        } else {
+            info["requests"] += c
+            info["associations"] = c;
+        }
+    });
+
+    await School.countDocuments({}, function(err, c) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err.message)
+        } else {
+            info["requests"] += c
+            info["school"] = c;
+        }
+    });
+
+    res.status(200).send(info);
+
+}
---
+++ routes/api/requests.routes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+
+// farmers requests
+
+//include Controller 
+const farmerController = require('../../controller/farmerController');
+
+//get Farmers
+router.get('/farmers/', farmerController.getFarmers);
+
+//get Farmer by id
+router.get('/farmers/:id', farmerController.getFarmerById);
+
+//add a Farmer
+router.post('/farmers/', farmerController.addFarmer);
+
+//edit a farmer request
+router.put('/farmers/:id', farmerController.updateFarmer);
+
+//delete a Farmer
+router.delete("/farmers/:id", farmerController.deleteFarmer);
+
+
+// associations requests
+
+//include Controller 
+const associationController = require('../../controller/associationController');
+
+//get associations
+router.get('/associations/', associationController.getAssociations);
+
+//get association by id
+router.get('/associations/:id', associationController.getAssociationById);
+
+//add a association
+router.post('/associations/', associationController.addAssociation);
+
+//edit a association request
+router.put('/associations/:id', associationController.updateAssociation);
+
+//delete a association
+router.delete("/associations/:id", associationController.deleteAssociation);
+
+
+// schools requests
+
+//include Controller 
+const schoolController = require('../../controller/schoolController');
+
+//get School
+router.get('/schools/', schoolController.getSchools);
+
+//get Farmer by id
+router.get('/schools/:id', schoolController.getSchoolById);
+
+//add a Farmer
+router.post('/schools/', schoolController.addSchool);
+
+//edit a farmer request
+router.put('/schools/:id', schoolController.updateSchool);
+
+//delete a Farmer
+router.delete("/schools/:id", schoolController.deleteSchool);
+
+
+// requests statistics
+
+//include Controller 
+const requestController = require('../../controller/requestController');
+
+router.get('/statistics', requestController.getStatistics)
+
+
+module.exports = router;
