06c457a80bcb6fe20f65a5b7c0c87fe1c9cffdc2
zarrix <54173583+zarrix@users.noreply.github.com>
2022-01-09 23:56:36 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://souf:zarr@pfacluster.7gnl9.mongodb.net/pfa_db?retryWrites=true&w=majority',
+    mongoURI: 'MongoDB URI Goes Here',
     TOKEN_SECRET: '6464df353353dgte35etretert3riti53fk3534242rft3343ruutozrioeioze3535fdtr43tdty',
     TOKEN_ADMIN: '6464df353353dgte35etretert3riti53fk3534242rft3343ruutozrioeioze3535fdtr4admin'
 }
